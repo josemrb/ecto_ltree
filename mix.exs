3b87cf4d69add9a1f3bc0957e67c66bdec313dd3
Jose Miguel Rivero Bruno <josemrb@users.noreply.github.com>
2019-04-16 11:08:20 -0400

    Update mix.exs bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoLtree.MixProject do
   def project do
     [
       app: :ecto_ltree,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
