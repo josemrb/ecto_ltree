dc97213afe87194a21564d28b993c4145933f020
Matthew Johnston <warmwaffles@gmail.com>
2022-03-28 21:11:23 -0500

    Refactor and add more tests for LabelTree

--- test/ecto_label_tree/label_tree_test.exs
+++ test/ecto_label_tree/label_tree_test.exs
@@ -1,32 +1,70 @@
 defmodule EctoLtree.LabelTreeTest do
   use EctoLtree.EctoCase, async: true
-  alias EctoLtree.LabelTree, as: Ltree
 
-  describe "Ltree.cast/1" do
+  alias EctoLtree.LabelTree
+
+  describe "cast/1" do
     test "top label" do
-      {:ok, result} = Ltree.cast("top")
-      assert %Ltree{} = result
-      assert 1 = length(result.labels)
+      assert {:ok, result} = LabelTree.cast("top")
+      assert %LabelTree{labels: ["top"]} == result
     end
 
     test "more than one label" do
-      {:ok, result} = Ltree.cast("top.countries.south_america")
-      assert %Ltree{} = result
-      assert 3 = length(result.labels)
+      assert {:ok, result} = LabelTree.cast("top.countries.south_america")
+      assert %LabelTree{labels: ["top", "countries", "south_america"]} == result
+    end
+
+    test "label with a space in it" do
+      assert :error == LabelTree.cast("invalid chars")
+    end
+
+    test "label with a wild card in it" do
+      assert :error == LabelTree.cast("top.*.bottom")
+    end
+
+    test "label with ❤️" do
+      assert :error == LabelTree.cast("❤️")
     end
 
     test "label chars" do
-      assert {:ok, %Ltree{}} = Ltree.cast("411_valid_chars")
-      assert :error == Ltree.cast("invalid chars")
+      assert {:ok, result} = LabelTree.cast("411_valid_chars")
+      assert %LabelTree{labels: ["411_valid_chars"]} == result
     end
 
-    test "label length" do
-      long_label = String.duplicate("long", 64)
-      assert {:ok, %Ltree{}} = Ltree.cast(long_label)
+    test "label length right at the max" do
+      long_label = String.duplicate("a", 256)
+      assert {:ok, %LabelTree{}} = LabelTree.cast(long_label)
+    end
 
-      # 260
-      too_long_label = String.duplicate("too_long", 33)
-      assert :error == Ltree.cast(too_long_label)
+    test "label length is too long" do
+      too_long_label = String.duplicate("a", 257)
+      assert :error == LabelTree.cast(too_long_label)
+    end
+
+    test "returns the struct passed" do
+      ltree = %LabelTree{labels: ["level1", "level2"]}
+
+      assert {:ok, result} = LabelTree.cast(ltree)
+      assert result == ltree
+    end
+  end
+
+  describe "dump/1" do
+    test "dumps the label tree" do
+      ltree = %LabelTree{labels: ["level1", "level2"]}
+      assert {:ok, "level1.level2"} == LabelTree.dump(ltree)
+    end
+  end
+
+  describe "load/1" do
+    test "loads the path from postgres" do
+      assert {:ok, %LabelTree{labels: ["level1", "level2"]}} == LabelTree.load("level1.level2")
+    end
+  end
+
+  describe "type/0" do
+    test "returns :ltree" do
+      assert LabelTree.type() == :ltree
     end
   end
 end
