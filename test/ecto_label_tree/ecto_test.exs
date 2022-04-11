884d713212fcbf12250782a36c60f1c8e434ffdb
Matthew Johnston <warmwaffles@gmail.com>
2022-04-11 17:09:49 -0500

    Cleans up some tests
    * Add Test context for tests
    * Remove duplicate test case
    * Assert results for postgrex tests
    * Refactor
    
    Co-authored-by: josemrb <work@josemrb.com>

--- test/ecto_label_tree/ecto_test.exs
+++ test/ecto_label_tree/ecto_test.exs
@@ -1,28 +1,17 @@
 defmodule EctoLtree.EctoTest do
   use EctoLtree.EctoCase, async: true
   alias EctoLtree.Item
+  alias EctoLtree.TestContext
 
   describe "Ecto integration" do
     test "can insert record" do
-      record = Item.changeset(%Item{}, %{path: "this.is.the.one"})
-      assert {:ok, _schema} = Repo.insert(record)
+      assert {:ok, _schema} = TestContext.create_item("this.is.the.one")
     end
 
     test "can load record" do
-      %Item{}
-      |> Item.changeset(%{path: "this.is.the.one"})
-      |> Repo.insert!()
+      TestContext.create_item("this.is.the.one")
 
       assert 0 < Repo.one(from(i in Item, select: count(i.id)))
-    end
-
-    test "can query record" do
-      %Item{}
-      |> Item.changeset(%{path: "this.is.the.one"})
-      |> Repo.insert!()
-
-      query = from(i in Item, select: count(i.id))
-      assert 0 < Repo.one(query)
     end
   end
 end
