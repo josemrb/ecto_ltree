9d13b5a7e5757ec90329d94064a0c946ce1cf9ca
Greg Rychlewski <grychle@LLL7WPM64V5M.ngco.com>
2022-12-19 23:50:55 -0500

    format

--- test/ecto_label_tree/postgrex_test.exs
+++ test/ecto_label_tree/postgrex_test.exs
@@ -9,9 +9,7 @@ defmodule EctoLtree.PostgrexTest do
   test "insert item", context do
     pid = context[:pid]
     path = "this.is.the.path"
-
     {:ok, _} = Postgrex.query(pid, "INSERT INTO items (path) VALUES ($1)", [path])
-
     {:ok, result} = Postgrex.query(pid, "SELECT * FROM items", [])
     [[_, result_path, _]] = result.rows
 
@@ -23,9 +21,7 @@ defmodule EctoLtree.PostgrexTest do
     pid = context[:pid]
     root = "this.is"
     path = root <> ".the.path"
-
     {:ok, _} = Postgrex.query(pid, "INSERT INTO items (path) VALUES ($1)", [path])
-
     {:ok, result} = Postgrex.query(pid, "SELECT * FROM items WHERE path <@ $1;", [root])
     [[_, result_path, _]] = result.rows
 
